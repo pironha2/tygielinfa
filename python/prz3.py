5a58a75b17d8c6ec288f451af18a2f7f4fdb67ca
Szaszkk <82641010+pironha2@users.noreply.github.com>
2026-04-21 11:19:15 +0200

    Create prz3.py

---
+++ python/prz3.py
@@ -0,0 +1,16 @@
+a = 5
+print("a = ", a)
+if (4<=a<=6):
+    print("wart zmiennej między 4 a 6")
+
+a = 5
+b = 5
+print("a = ", a, ", b = ", b)
+if (a==b):
+    print("wart zmiennych są równe")
+
+a = 5
+b = 6
+print("a = ", a, ", b = ", b)
+if (a != b):
+    print("wart zmiennych są różne")

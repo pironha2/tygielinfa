e65b28185c956dfac47840cb0868b453e2683dc7
Szaszkk <82641010+pironha2@users.noreply.github.com>
2026-05-12 11:25:43 +0200

    Create prz9.py

---
+++ python/prz9.py
@@ -0,0 +1,18 @@
+print("podaj skibidi numer 1: ")
+a = int(input())
+print("podaj skibidi numer 2: ")
+b = int(input())
+c = a + b
+print(c, "\n", a, " + ", b, " = ", c)
+c = a - b
+print(c, "\n", a, " - ", b, " = ", c)
+c = a * b
+print(c, "\n", a, " * ", b, " = ", c)
+c = a ** b
+print(c, "\n", a, " ** ", b, " = ", c)
+c = a / b
+print(c, "\n", a, " / ", b, " = ", c)
+c = a // b
+print(c, "\n", a, " // ", b, " = ", c)
+c = a % b
+print(c, "\n", a, " % ", b, " = ", c)
